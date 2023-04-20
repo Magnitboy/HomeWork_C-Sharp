e37cfca7424de6aadf369a2e31ee92a7c6de7aaf
Андрей <magnitboy.mgn@yandex.ru>
2023-04-20 09:27:51 +0500

    Семинар прошел продуктивно

---
+++ Seminar4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Seminar 4.
+
+int[] CreateRandomArrey(int size, int minValue, int maxValue)
+{
+    int[] arrey = new int[size];
+
+    for(int i = 0; i < size; i++)
+        arrey[i] = new Random().Next(minValue, maxValue + 1);
+
+    return arrey;
+}
+
+void ShowArrey(int[] arrey)
+{
+    for(int i = 0; i < arrey.Length; i++)
+        Console.Write(arrey[i] + " ");
+    
+    Console.WriteLine();
+}
+
+Console.Write("Input a size for arrey: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArrey = CreateRandomArrey(size, min, max);
+ShowArrey(newArrey);
+
+
